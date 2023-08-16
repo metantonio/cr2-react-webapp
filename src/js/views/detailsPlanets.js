0cb7e5ff7ff372d28040187e4f406fd73c312042
Ashley Molina <131078848+Ashley8-prog@users.noreply.github.com>
2023-08-16 03:03:41 +0000

    Cambios 15 agosto fin

--- src/js/views/detailsPlanets.js
+++ src/js/views/detailsPlanets.js
@@ -9,8 +9,12 @@ export const DetailsPlanets = props => {
     const params = useParams();
     return (
         <div className="jumbotron">
-
+            <img className="card-img-top more-info-img" key={params.theid} src={'https://starwars-visualguide.com/assets/img/planets/' + (parseInt(params.theid) + 1) + '.jpg'} onError={({ currentTarget }) => {
+                currentTarget.onerror = null; // prevents looping
+                currentTarget.src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+            }} alt="Card image cap" />
             <h1>{store.planetsStarWars.length != 0 ? store.planetsStarWars[params.theid].name : 'Loading'}</h1>
+            <p>{store.planetsStarWars.length != 0 ? '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ' : 'Loading'}</p>
             <div className="Extra-info">
                 <h3>{store.planetsStarWars.length != 0 ? "Rotation Period:" + " " + store.planetsStarWars[params.theid].rotation_period : 'Loading'}</h3>
                 <h3>{store.planetsStarWars.length != 0 ? "Orbital Period:" + " " + store.planetsStarWars[params.theid].orbital_period : 'Loading'}</h3>
--- src/js/views/detailsVehicles.js
+++ src/js/views/detailsVehicles.js
@@ -9,8 +9,12 @@ export const DetailsVehicles = props => {
     const params = useParams();
     return (
         <div className="jumbotron">
-
+            <img className="card-img-top more-info-img" key={params.theid} src={'https://starwars-visualguide.com/assets/img/vehicles/' + (parseInt(params.theid) + 1) + '.jpg'} onError={({ currentTarget }) => {
+                currentTarget.onerror = null; // prevents looping
+                currentTarget.src = "https://starwars-visualguide.com/assets/img/placeholder.jpg";
+            }} alt="Card image cap" />
             <h1>{store.vehiclesStarWars.length != 0 ? store.vehiclesStarWars[params.theid].name : 'Loading'}</h1>
+            <p>{store.vehiclesStarWars.length != 0 ? '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. ' : 'Loading'}</p>
             <div className="Extra-info">
                 <h3>{store.vehiclesStarWars.length != 0 ? "Height:" + " " + store.vehiclesStarWars[params.theid].model : 'Loading'}</h3>
                 <h3>{store.vehiclesStarWars.length != 0 ? "Mass:" + " " + store.vehiclesStarWars[params.theid].manufacturer : 'Loading'}</h3>
